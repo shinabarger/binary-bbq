1dae4b51dfab0e9c75298ec1a00e228d9e255e44
shinabarger <davidshinabarger@gmail.com>
2017-03-18 14:40:48 -0400

    add all 3 files

---
+++ index.js
@@ -0,0 +1,17 @@
+var bbqOrder = []
+
+function display() {
+    for (var i = 0; i < bbqOrder.length; i++) {
+        document.write(bbqOrder[i] + "<br/>");
+        console.log(bbqOrder[i] + "<br/>");
+    }
+}
+
+function Showorder() {
+    var bbqOrder = [];
+    for (var i = 0; i < bbqOrder.length; i++) {
+        document.write(bbqOrder[i] + "<br/>");
+        console.log(bbqOrder[i] + "<br/>");
+    }
+    document.getElementById("order").innerHTML = "ITEM";
+}
